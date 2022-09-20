7f8923bb6f0df866c10744edfad6f70bfe6c8725
chetannn <chetankharel7@gmail.com>
2022-09-20 12:34:19 +0545

    setup: prisma adapter for next auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,12 @@
 import NextAuth from "next-auth"
+import { PrismaClient } from "@prisma/client"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+
+const prisma = new PrismaClient()
+
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   callbacks: {
     session({ session, token, user }) {
       return session // The return type will match the one returned in `useSession()`
