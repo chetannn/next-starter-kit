6d78f99dd02f2a43dd1fc92f0362824d50f81c0a
chetannn <chetankharel7@gmail.com>
2022-09-22 12:32:53 +0545

    ui: dropdown and headless ui

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,56 @@
-import Sidebar from "./Sidebar";
+import Sidebar from './Sidebar'
+import { Menu, Transition } from '@headlessui/react'
+import React, { Fragment } from 'react'
+import Link from 'next/link';
 
 export default function Layout(props) {
   return (
     <div className='flex min-h-screen antialiased'>
-        <Sidebar />
+      <Sidebar />
       <div className='flex-1'>
         <nav className='flex py-3 px-6 justify-between items-center bg-white shadow'>
           <div></div>
-          <div className='bg-red-400 rounded-full h-8 w-8'></div>
+
+          <Menu as="div">
+            <Menu.Button>
+              <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+            </Menu.Button>
+
+            <Transition
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95">
+              <Menu.Items className="absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <Menu.Item as={Fragment}>
+                  {({ active }) => (
+                    <Link href="/profile">
+                      <a className={`${active ? 'bg-gray-100' : ''} block px-4 py-2 text-gray-700 text-sm`}>Your Profile</a>
+                    </Link>
+                  )}
+                </Menu.Item>
+
+                <Menu.Item>
+                  {({ active }) => (
+                    <Link href='/settings'>
+                      <a className={`${active ? 'bg-gray-100' : ''} block px-4 py-2 text-gray-700 text-sm`}>Settings</a>
+                    </Link>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <Link href="/logout">
+                      <a className={`${active ? 'bg-gray-100' : ''} block px-4 py-2 text-gray-700 text-sm`}>Logout</a>
+                    </Link>
+                  )}
+                </Menu.Item>
+              </Menu.Items>
+            </Transition>
+          </Menu>
         </nav>
+        
         <main className='px-4 pt-4'>{props.children}</main>
       </div>
     </div>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,27 +1,29 @@
 import React from 'react'
-import { HomeIcon  } from '@heroicons/react/24/outline'
-import { Cog6ToothIcon  } from '@heroicons/react/24/outline'
+import { HomeIcon } from '@heroicons/react/24/outline'
+import { Cog6ToothIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link'
 
-const Sidebar : React.FC = () => {
+const Sidebar: React.FC = () => {
   return (
     <aside className='h-screen w-64 bg-gray-800'>
-       <div className='px-3 pt-4 flex flex-col space-y-4'>
-          
-         <div className='p-2 flex rounded items-center space-x-2 hover:bg-gray-900'>
-         <HomeIcon className='h-5 w-5 text-gray-200'/>
-         <p className='tracking-widest text-sm text-gray-200'>
-                Dashboard
+      <div className='px-3 pt-4 flex flex-col space-y-4'>
+
+        <div className='p-2 flex rounded items-center space-x-2 hover:bg-gray-900'>
+          <HomeIcon className='h-5 w-5 text-gray-200' />
+          <p className='tracking-widest text-sm text-gray-200'>
+            <Link href="/dashboard">Dashboard</Link>
           </p>
-           </div>
+        </div>
 
+        <div className='p-2 rounded flex items-center space-x-2 hover:bg-gray-900'>
+          <Cog6ToothIcon className='h-5 w-5 text-gray-200' />
+          <p className='tracking-widest text-sm text-gray-200'>
+            <Link href="/settings">Settings</Link>
+          </p>
+        </div>
 
-         <div className='p-2 rounded flex items-center space-x-2 hover:bg-gray-900'>
-         <Cog6ToothIcon className='h-5 w-5 text-gray-200'/>
-         <p className='tracking-widest text-sm text-gray-200'>Settings</p>
-           </div>
-
-         </div>
-     </aside>
+      </div>
+    </aside>
   )
 }
 
