8dcc0cadb6cedeaa0ddf06acfb2d349783840829
chetannn <chetankharel7@gmail.com>
2022-10-01 09:50:18 +0545

    active links

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,12 +2,17 @@ import React from 'react'
 import { HomeIcon } from '@heroicons/react/24/outline'
 import { Cog6ToothIcon } from '@heroicons/react/24/outline'
 import AppLink from './AppLink'
+import { useRouter } from 'next/router'
 
 const Sidebar: React.FC = () => {
+
+  const router = useRouter()
+
   return (
     <aside className='h-screen w-64 bg-gray-800'>
       <div className='px-6 pt-12 flex flex-col space-y-8'>
-        <div className='p-2 hover:bg-gray-900 rounded-lg'>
+
+        <div className={`${router.pathname === '/dashboard' ? 'bg-gray-900' : ''} p-2 hover:bg-gray-900 rounded-lg w-full flex items-center space-x-2`}>
           <AppLink className="w-full flex items-center space-x-2" href="/dashboard">
             <HomeIcon className='h-5 w-5 text-gray-200' />
             <p className='text-gray-200'>
@@ -16,7 +21,7 @@ const Sidebar: React.FC = () => {
           </AppLink>
         </div>
 
-        <div className='p-2 rounded flex items-center space-x-2 hover:bg-gray-900'>
+        <div  className={`${router.pathname === '/settings' ? 'bg-gray-900' : ''} p-2 hover:bg-gray-900 rounded-lg w-full flex items-center space-x-2`}>
                 <AppLink className="w-full flex items-center space-x-2" href="/settings">
                   <Cog6ToothIcon className='h-5 w-5 text-gray-200' />
                       <p className='text-gray-200'>
